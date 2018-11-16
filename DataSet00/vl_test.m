07bcd819c5ca873d28ac222d7fbdff03c295d8d3
briannaburton1 <briannaburton@gmail.com>
2018-11-16 20:17:37 +0100

    initial commit

---
+++ DataSet00/vl_test.m
@@ -0,0 +1,37 @@
+addpath('/Users/briannaburton/vlfeat-0.9.21/toolbox/demo');
+
+Ia = imread('skin1.jpg') ;
+Ib = imread('skin2.jpg') ;
+
+[fa,da] = vl_sift(im2single(rgb2gray(Ia))) ;
+[fb,db] = vl_sift(im2single(rgb2gray(Ib))) ;
+
+[matches, scores] = vl_ubcmatch(da,db) ;
+
+[drop, perm] = sort(scores, 'descend') ;
+matches = matches(:, perm) ;
+scores  = scores(perm) ;
+
+figure(1) ; clf ;
+imagesc(cat(2, Ia, Ib)) ;
+axis image off ;
+vl_demo_print('sift_match_1', 1) ;
+
+figure(2) ; clf ;
+imagesc(cat(2, Ia, Ib)) ;
+
+xa = fa(1,matches(1,:)) ;
+xb = fb(1,matches(2,:)) + size(Ia,2) ;
+ya = fa(2,matches(1,:)) ;
+yb = fb(2,matches(2,:)) ;
+
+hold on ;
+h = line([xa ; xb], [ya ; yb]) ;
+set(h,'linewidth', 1, 'color', 'b') ;
+
+vl_plotframe(fa(:,matches(1,:))) ;
+fb(1,:) = fb(1,:) + size(Ia,2) ;
+vl_plotframe(fb(:,matches(2,:))) ;
+axis image off ;
+
+vl_demo_print('sift_match_2', 1) ;
---
+++ computeHomography.m
@@ -0,0 +1,14 @@
+function H = computeHomography(Features, Matches, Model)
+    % Features: Nx2 matrices storing the features [x, y]
+    % Matches: Nx2 matrices storing the correspondences [x?, y?]
+    % Model: string specifying a transformation model
+    
+    if Model == "Projective"
+        
+        
+    end
+    
+
+
+
+end
