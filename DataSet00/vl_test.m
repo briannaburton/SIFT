f862506f18bfba4c57a952a12bd7c29fbb0c30f2
OlehKSS <olehkss@github.com>
2018-12-02 22:04:23 +0100

    New pictures for the report were added.

--- DataSet00/vl_test.m
+++ DataSet00/vl_test.m
@@ -1,10 +1,10 @@
-addpath('/Users/briannaburton/vlfeat-0.9.21/toolbox/demo');
+%addpath('/Users/briannaburton/vlfeat-0.9.21/toolbox/demo');
 
 
 Ia = imread('retina1.png') ;
 Ib = imread('retina2.png') ;
 
-peak_thresh = 0;
+peak_thresh = 0.021;
 edge_thresh = 10;
 
 [fa,da] = vl_sift(im2single(rgb2gray(Ia)), 'PeakThresh', peak_thresh) ;
@@ -17,12 +17,12 @@ matches = matches(:, perm) ;
 scores  = scores(perm) ;
 
 figure(1) ; clf ;
-imagesc(cat(2, Ia, Ib)) ;
+imshow(cat(2, rgb2gray(Ia), rgb2gray(Ib))) ;
 axis image off ;
 vl_demo_print('sift_match_1', 1) ;
 
 figure(2) ; clf ;
-imagesc(cat(2, Ia, Ib)) ;
+imshow(cat(2, rgb2gray(Ia), rgb2gray(Ib))) ;
 
 xa = fa(1,matches(1,:)) ;
 xb = fb(1,matches(2,:)) + size(Ia,2) ;
